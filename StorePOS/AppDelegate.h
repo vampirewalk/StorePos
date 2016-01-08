c8962e2a1235defd0e49ce5fdebdafbbb37066bb
vampirewalk <vampirewalk@gmail.com>
2016-01-08 10:23:00 +0800

    Initial Commit

---
+++ StorePOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StorePOS
+//
+//  Created by KevinShen on 2016/1/8.
+//  Copyright © 2016年 mocacube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StorePOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StorePOS
+//
+//  Created by KevinShen on 2016/1/8.
+//  Copyright © 2016年 mocacube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
