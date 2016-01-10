14af69f00c880525e258cb766341b74f940df984
vampirewalk <vampirewalk@gmail.com>
2016-01-10 21:25:26 +0800

    Add VIewController skeleton.

---
+++ StorePOS/EditOrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditOrderViewController.h
+//  StorePOS
+//
+//  Created by vampirewalk on 2016/1/10.
+//  Copyright © 2016年 mocacube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditOrderViewController : UIViewController
+
+@end
